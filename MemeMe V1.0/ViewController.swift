6799cced86a5ce4004cf6589d492571beeb6f80e
Jeremy Kievit <jeremyjkievit@gmail.com>
2020-08-24 21:43:48 -0700

    Initial Commit

---
+++ MemeMe V1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe V1.0
+//
+//  Created by admin on 8/24/20.
+//  Copyright © 2020 Com.JeremyKievit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
